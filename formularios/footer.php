3e83b194bb77eb00562f86cbb01d8356539b5534
mboisselier <mboisselier@jusrionegro.gov.ar>
2019-04-09 14:38:03 -0300

    limpieza

--- formularios/footer.php
+++
@@ -1,6 +0,0 @@
-        </div> <!-- Fin del contenedor -->
-        <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/scripts.js"></script>
-    </body>
-</html>
